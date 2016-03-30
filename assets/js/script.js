d24abce0d28d1c2ae3ea404a9e39c2d9c88823b1
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-30 16:49:37 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,31 @@
 $(function(){
   
-  var input = "National Great Blacks In Wax Museum";
-  input = input.replace(" ", "+");
+  var string = "National Great Blacks In Wax Museum";
+  string = input.replace(" ", "+");
+  
+  wikiSearch(string);
   
   // this funnction returns an array using the wiki API
-  $.ajax({
-    url:'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=' + input + '&callback=?',
-    data: {format: 'json'},
-    dataType: 'jsonp'
-  }).done( function ( data ) {
-    var searchArray = data.query.search;
-    var div = $("#results");
-    var wiki = 'https://en.wikipedia.org/wiki/';
-    
-    searchArray.forEach(function(entry) {
-    console.log(entry.title);
-    
-    var title = entry.title.replace(" ", "");
-    
-    div.append('<div><a href="' + wiki + title + '" target="_blank">' + entry.title + '</a></div>');
-    });
-
-  });
+  function wikiSearch(input){
+    $.ajax({
+      url:'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=' + input + '&callback=?',
+      data: {format: 'json'},
+      dataType: 'jsonp'
+    }).done( function ( data ) {
+      var searchArray = data.query.search;
+      var div = $("#results");
+      var wiki = 'https://en.wikipedia.org/wiki/';
+      
+      searchArray.forEach(function(entry) {
+      console.log(entry.title);
+      
+      var title = entry.title.replace(" ", "");
+      
+      div.append('<div><a href="' + wiki + title + '" target="_blank">' + entry.title + '</a></div>');
+      });
   
+    });
+  }
   // https://en.wikipedia.org/wiki/
  
 });
