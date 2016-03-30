d1be1d6c3e16e86eb36fd6b54566f9ab18a36f02
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-30 12:58:07 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,62 @@ $(function(){
     }
   });  
 
+  var mw;
+  (function (mw) {
+  
+    /**
+     * Query a MediaWiki api.php instance with the given options
+     */
+    function mwQuery(endpoint, options) {
+  
+      /**
+       * Create a uniquely-named callback that will process the JSONP results
+       */
+      var createCallback = function (k) {
+        var i = 1;
+        var callbackName;
+        do {
+          callbackName = 'callback' + i;
+          i = i + 1;
+        } while (window[callbackName])
+        window[callbackName] = k;
+        return callbackName;
+      }
+  
+      /**
+       * Flatten an object into a URL query string.
+       * For example: { foo: 'bar', baz: 42 } becomes 'foo=bar&baz=42'
+       */
+      var queryStr = function (options) {
+        var query = [];
+        for (var i in options) {
+          if (options.hasOwnProperty(i)) {
+            query.push(encodeURIComponent(i) + '=' + encodeURIComponent(options[i]));
+          }
+        }
+        return query.join('&');
+      }
+  
+      /**
+       * Build a function that can be applied to a callback.  The callback processes
+       * the JSON results of the API call.
+       */
+      return function (k) {
+        options.format = 'json';
+        options.callback = createCallback(k);
+        var script = document.createElement('script');
+        script.src = endpoint + '?' + queryStr(options);
+        var head = document.getElementsByTagName('head')[0];
+        head.appendChild(script);
+      };
+  
+    }
+  
+    mw.api = {
+      query: mwQuery,
+    };
+  
+  })(mw || (mw = {}));
+
+  
 });
