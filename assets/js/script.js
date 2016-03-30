757fd8b71946c5716b85c9e854cc309773db69dc
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-30 16:09:44 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 $(function(){
   
+  var input = "National Great Blacks In Wax Museum";
+  input = input.replace(" ", "+");
+  
   $.ajax({
     //url: '//www.mediawiki.org/w/api.php?format=jsonty&action=query&meta=siteinfo&siprop=general&callback=?',
     //url:'//www.mediawiki.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein&callback=?',
-    url:'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein&callback=?',
+    url:'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch='+ input + '&callback=?',
     data: {format: 'json'},
     dataType: 'jsonp'
-  }).done( function ( data ) {console.log(data.query.search[0].title)});
+  }).done( function ( data ) {console.log(data.query.search.[0].title)});
   
   // https://en.wikipedia.org/wiki/
  
