a87f7a9ed7f44acc0ba9a4496120926dcee383f5
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-30 12:49:09 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   
-  
+  $.ajax({
+    url: '//www.wikidata.org/w/api.php',
+    data: { action: 'wbgetentities', ids: mw.config.get('wgWikibaseItemId'), format: 'json' },
+    dataType: 'jsonp',
+    success: function (x) {
+      console.log('wb label', x.entities.Q39246.labels.en.value);
+      console.log('wb description', x.entities.Q39246.descriptions.en.value);
+    }
+  });  
 
 });
