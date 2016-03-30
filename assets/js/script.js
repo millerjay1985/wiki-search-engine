12e6069fbe3f604722d0e193aad47229d608dc25
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-30 15:43:15 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 $(function(){
   
   $.ajax({
-    url: '//www.mediawiki.org/w/api.php?format=jsonty&action=query&meta=siteinfo&siprop=general&callback=?',
+    //url: '//www.mediawiki.org/w/api.php?format=jsonty&action=query&meta=siteinfo&siprop=general&callback=?',
+    url:'//www.mediawiki.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein&callback=?',
     data: {format: 'json'},
     dataType: 'jsonp'
   }).done( function ( data ) {console.log(data)});
+  
+  // https://en.wikipedia.org/wiki/
  
 });
